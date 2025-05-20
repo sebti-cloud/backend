1ce833b97d1dc233e8903a01ed9446186a6f9018
sebti-cloud <lahrechsimo732@gmail.com>
2025-05-20 21:03:25 +0200

    db.js

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ma_base_de_donnees',
-  password: 'pwd123',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Obligatoire pour Railway
 });
 
 client.connect().catch(err => console.error('Connection error', err.stack));
